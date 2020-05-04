0f38a13fba6fdc08bbad2ca98ab929807c9dd710
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-05-04 05:55:48 +0200

    Reduce message snackbar to only show one message text since all validation also happens in the frontend

--- src/Components/MessageSnackbar.jsx
+++ src/Components/MessageSnackbar.jsx
@@ -9,44 +9,8 @@ import useStyles from "../styles";
 function MessageSnackbar(props) {
     const classes = useStyles();
 
-    let text = "";
-
-    switch (props.text) {
-
-        // REGULAR FORM ERRORS (Client/Server)
-        case "validation error: name/email missing":
-            // Email/Password/Zip Code/Country form field empty
-            text = "Bitte trage Namen und Email Addresse ein";
-            break;
-        case "validation error: email domain":
-            text = "Du benötigst eine \"...@tum.de\" oder eine \"...@mytum.de\" Email Adresse";
-            break;
-        case "validation error: email format":
-            text = "Bitte trage ein gültige Adresse ein";
-            break;
-        case "validation error: street/zip_code/city missing":
-            text = "Um dir das Kit zu schicken, benötigen wir deine ganze Adresse."
-            break;
-
-        case "validation error: country invalid":
-            text = "Bitte nicht versuchen, invalide Daten zu senden, du Schlingel..."
-            break;
-        case "validation error: XSS alert":
-            text = "Bitte nicht versuchen, invalide Daten zu senden, du Schlingel..."
-            break;
-
-        case "":
-            break;
-
-        default:
-            text = "Leid trat auf unserem Server ein Fehler auf, bitte überprüfe deine " +
-                "Eingabe oder versuche es später erneut."
-            break;
-    }
-
-    if (text !== "") {
-        console.log("Error message: '" + props.text + "'");
-    }
+    const text = "Leid trat auf unserem Server ein Fehler auf, bitte überprüfe deine " +
+        "Eingabe oder versuche es später erneut.";
 
     return (
         <Snackbar className={classes.snackbar}
