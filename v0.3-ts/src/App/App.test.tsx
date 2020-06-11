7377de97f2592269771730709425bffc5d94712e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-06-11 19:51:00 +0200

    Restructure src directory from template

--- v0.3-ts/src/App/App.test.tsx
+++ v0.3-ts/src/App/App.test.tsx
@@ -1,9 +1,12 @@
+
+// @ts-ignore
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
+  // @ts-ignore
   expect(linkElement).toBeInTheDocument();
 });
