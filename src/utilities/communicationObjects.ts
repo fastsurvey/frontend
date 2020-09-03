997bbcf50bbe70ec94bf9e1efedc5a0a2ca8c9fb
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-09-04 00:00:26 +0200

    Add functions to generate the initial formData

---
+++ src/utilities/communicationObjects.ts
@@ -0,0 +1,35 @@
+
+import { ConfigInterface } from './fieldTypes';
+
+export function generateInitialFormData(config: ConfigInterface): object {
+    const formData: object = {};
+
+    config.fields.forEach((field, index) => {
+        let fieldData: object|string|undefined;
+        switch (field.type) {
+            case 'Radio':
+            case 'Selection':
+                fieldData = {};
+                field.properties.fields.forEach((option, innerIndex) => {
+                    Object.assign(fieldData, {
+                        [(innerIndex + 1).toString()]: false,
+                    });
+                });
+                break;
+            case 'Text':
+                fieldData = '';
+                break;
+            default:
+                fieldData = undefined;
+                break;
+        }
+
+        if (fieldData !== undefined) {
+            Object.assign(formData, {
+                [(index + 1).toString()]: fieldData,
+            });
+        }
+    });
+
+    return formData;
+}
