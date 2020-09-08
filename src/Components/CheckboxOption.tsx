39050b3b97e95a17b234f51328f11588063c53dd
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-09-09 00:59:38 +0200

    Add SelectionField component

---
+++ src/Components/CheckboxOption.tsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import assert from 'assert';
+
+interface CheckboxOptionProps {
+    label: string;
+    checked: any;
+    onChange?(newValue: boolean): void;
+}
+
+function CheckboxOption(props: CheckboxOptionProps) {
+
+    function handleChange(newValue: boolean) {
+        if (props.onChange !== undefined) {
+            props.onChange(newValue);
+        }
+    }
+
+    // Dynamic type assertion without using typescript because
+    // the formData object is being created dynamically
+    assert(
+        typeof props.checked === 'boolean',
+        'param checked has to be of type boolean'
+    );
+
+    return (
+        <div>
+            <label>{props.label}</label>
+            <input
+                type='checkbox'
+                checked={props.checked}
+                onChange={event => handleChange(event.target.checked)}
+            />
+        </div>
+    );
+}
+
+export default CheckboxOption;
