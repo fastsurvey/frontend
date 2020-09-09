48cfb6c8add26833d4a05fdc10b390d794aee8e1
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-09-09 23:36:45 +0200

    Add TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './tailwind.out.css';
 import * as serviceWorker from './serviceWorker';
 import ReduxWrapper from './App/Wrapper/ReduxWrapper';
 import Router from './App/Wrapper/Router';
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    purge: {
+        enabled: false,
+        content: [
+            './src/**/*.js',
+            './src/**/*.jsx',
+            './src/**/*.ts',
+            './src/**/*.tsx',
+            './public/**/*.html',
+        ],
+    }
+};
