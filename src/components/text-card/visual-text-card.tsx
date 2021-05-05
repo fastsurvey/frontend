120b58acd182e04b6e9c2380c631710947444eaf
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-05-05 22:25:00 +0200

    Implement text card and index page

--- src/components/text-card/visual-text-card.tsx
+++ src/components/text-card/visual-text-card.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import 'styles/markdown.css';
 
-function VisualTextCard(props: {
-    title: string;
-    children: React.ReactNode | string;
-}) {
+// TODO: Replace links in text as well (starting with https:// or www.)
+
+function VisualTextCard(props: {title: string; text: string}) {
     return (
-        <div>
-            <div>{props.title}</div>
-            <div>{props.children}</div>
+        <div className='w-full p-6 bg-white rounded shadow centering-col'>
+            <div className='text-2xl text-black font-weight-600'>
+                {props.title}
+            </div>
+            {props.text.replace(/\s*/, '').length > 0 && (
+                <div
+                    className='mt-3 text-base text-gray-900 opacity-70 markdown font-weight-500'
+                    dangerouslySetInnerHTML={{
+                        __html: ''.concat(
+                            ...props.text
+                                .replaceAll(/<[^>]*>/g, '')
+                                .split('\n')
+                                .map((s) => `<p>${s}</p>`),
+                        ),
+                    }}
+                />
+            )}
         </div>
     );
 }
--- src/pages/survey-page/index.tsx
+++ src/pages/survey-page/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import {types} from 'types';
 import {connect} from 'react-redux';
+import VisualTextCard from 'components/text-card/visual-text-card';
+
+const LIPSUM =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis.\n Interdum velit euismod in pellentesque. Suspendisse interdum consectetur libero id faucibus. Commodo nulla facilisi nullam.';
 
 function SurveyIndexPage(props: {formConfig: types.SurveyConfig | undefined}) {
     if (!props.formConfig) {
         return <div />;
     }
 
-    return <div>Hello there</div>;
+    const config: types.SurveyConfig = props.formConfig;
+
+    return (
+        <div className='w-full max-w-xl'>
+            <VisualTextCard title={config.title} text={LIPSUM} />
+        </div>
+    );
 }
 
 const mapStateToProps = (state: types.ReduxState) => ({
