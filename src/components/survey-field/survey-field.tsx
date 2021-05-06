e63c1188df0b7af0d74ae512573bd82f8dbc0800
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-05-06 13:30:07 +0200

    Add red/green bar to survey-field

--- src/components/survey-field/survey-field.tsx
+++ src/components/survey-field/survey-field.tsx
@@ -11,16 +11,30 @@ function SurveyField(props: {
     const {fieldConfig, fieldIndex, formData, formValidation} = props;
 
     return (
-        <div className='w-full p-6 bg-white rounded shadow-md centering-col'>
-            <div className='text-2xl text-black font-weight-600'>
-                {fieldConfig.title}
+        <div className='w-full overflow-hidden rounded shadow-md centering-col'>
+            <div className='w-full p-6 bg-white shadow-md centering-col'>
+                <div className='w-full text-2xl text-left text-black font-weight-600'>
+                    {fieldIndex + 1}. {fieldConfig.title}
+                </div>
+                <div className='w-full mt-3 text-grey-900 markdown font-weight-500'>
+                    {fieldConfig.description}
+                </div>
+                {JSON.stringify(fieldConfig)},{' '}
+                {JSON.stringify(formData[fieldIndex + 1])},{' '}
+                {JSON.stringify(formValidation[fieldIndex + 1])}
             </div>
-            <div className='w-full mt-3 text-grey-900 markdown font-weight-500'>
-                {fieldConfig.description}
+            <div
+                className={
+                    'w-full px-6 py-3 shadow-md centering-row ' +
+                    (formValidation[fieldIndex + 1]
+                        ? 'text-green-600 bg-green-100 '
+                        : 'text-red-600 bg-red-100')
+                }
+            >
+                <div className='text-left flex-max font-weight-600 text-md'>
+                    Hello there
+                </div>
             </div>
-            {JSON.stringify(fieldConfig)},{' '}
-            {JSON.stringify(formData[fieldIndex + 1])},{' '}
-            {JSON.stringify(formValidation[fieldIndex + 1])}
         </div>
     );
 }
