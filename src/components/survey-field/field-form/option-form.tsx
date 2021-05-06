7b4889c50b8d0b44da68dbe7723b9b2d006cc2fa
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-05-06 14:01:41 +0200

    Implement option rendering

--- src/components/survey-field/field-form/option-form.tsx
+++ src/components/survey-field/field-form/option-form.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {types} from 'types';
+import {icons} from '../../../assets/icons/index';
 
 function OptionForm(props: {
     fieldConfig: types.SurveyField;
@@ -13,11 +14,16 @@ function OptionForm(props: {
             <div className='w-full text-2xl text-left text-black font-weight-600'>
                 {fieldIndex + 1}. {fieldConfig.title}
             </div>
-            <div className='w-full mt-3 text-grey-900 markdown font-weight-500'>
-                {fieldConfig.description}
+            <div className='w-full mt-3 space-x-2 flex-row-left'>
+                <div className='flex-shrink-0 w-10 h-10 p-1.5 cursor-pointer'>
+                    {formData[fieldIndex + 1]
+                        ? icons.checkboxFilled
+                        : icons.checkboxEmpty}
+                </div>
+                <div className='w-full text-grey-900 markdown font-weight-500'>
+                    {fieldConfig.description}
+                </div>
             </div>
-            {JSON.stringify(fieldConfig)},{' '}
-            {JSON.stringify(formData[fieldIndex + 1])},{' '}
         </>
     );
 }
