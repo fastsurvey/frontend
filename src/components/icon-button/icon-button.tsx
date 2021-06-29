1f497e552f40665086238ceb7e6a2f30bbe4fde5
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-06-29 04:19:46 +0200

    Add icon-button component from console

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -1,21 +1,46 @@
 import React from 'react';
 
-function IconButton(props: {text: string; icon: React.ReactNode}) {
+export default function IconButton(props: {
+    text: string;
+    icon?: React.ReactNode;
+    onClick?(): void;
+    variant?: 'flat-light-blue' | 'light-on-dark' | 'dark-on-dark';
+    disabled?: boolean;
+}) {
+    const {text, icon, onClick, variant, disabled} = props;
+
+    let variantClasses: string;
+    switch (variant) {
+        case 'flat-light-blue':
+            variantClasses = disabled
+                ? 'bg-gray-200 text-gray-400 icon-gray cursor-not-allowed '
+                : 'bg-blue-50 text-blue-900 icon-dark-blue';
+            break;
+        case 'light-on-dark':
+            variantClasses =
+                'bg-gray-200 hover:bg-gray-200 shadow text-gray-900 icon-blue';
+            break;
+        case 'dark-on-dark':
+            variantClasses = 'hover:bg-gray-800 text-gray-200 icon-blue';
+            break;
+        default:
+            variantClasses =
+                'bg-gray-100 hover:bg-blue-100 shadow text-gray-900 icon-blue';
+            break;
+    }
+
     return (
-        <div
+        <button
             className={
-                'h-10 rounded pl-2 pr-4 ' +
-                'centering-row cursor-pointer bg-blue-050 hover:bg-blue-100'
+                'p-0.5 rounded centering-row h-8 ' +
+                'no-selection ringable ' +
+                variantClasses
             }
+            onClick={onClick && !disabled ? onClick : () => {}}
+            disabled={disabled === true || !onClick}
         >
-            <div className='w-8 h-8 p-1 mr-1 icon-blue opacity-80'>
-                {props.icon}
-            </div>
-            <div className='text-base text-blue-900 font-weight-500'>
-                {props.text}
-            </div>
-        </div>
+            {icon && <div className='p-1 -mr-1.5 w-7 h-7'>{icon}</div>}
+            <div className={'font-weight-600 px-2 text-base'}>{text}</div>
+        </button>
     );
 }
-
-export default IconButton;
