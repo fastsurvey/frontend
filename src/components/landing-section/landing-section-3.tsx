f6b4dfdede6d8f16a45d0ba9a8710ebe709c6cca
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-07-01 01:29:33 +0200

    Stache stuff for cra/tailwindjit fix

--- src/components/landing-section/landing-section-3.tsx
+++ src/components/landing-section/landing-section-3.tsx
@@ -11,36 +11,46 @@ const BulletPoint = (props: {icon: React.ReactNode; text: string}) => (
 );
 export default function LandingSection3() {
     return (
-        <LandingPageSection
-            leftChild={
-                <div className='space-y-5 flex-col-left'>
-                    <div className='text-xl text-blue-100 uppercase'>
-                        <strong>Pricing</strong> should be transparent ...
-                    </div>
-                    <div className='space-y-3 text-base text-gray-100 flex-col-left'>
-                        <div>
-                            As many surveys as you want - billing per survey:
+        <div className='w-full min-h-[200vh] border-b-4 border-gray-800 centering-col'>
+            <div
+                className={
+                    'grid w-full px-8 py-16 ' +
+                    'grid-cols-1 gap-x-0 gap-y-8 ' +
+                    'lg:gap-y-0 lg:grid-cols-2 lg:gap-x-8 ' +
+                    'xl:gap-x-12'
+                }
+            >
+                <div className='w-full ml-auto lg:max-w-lg flex-col-left'>
+                    <div className='space-y-5 flex-col-left'>
+                        <div className='text-xl text-blue-100 uppercase'>
+                            <strong>Pricing</strong> should be transparent ...
                         </div>
-                        <BulletPoint
-                            icon={icons.discount}
-                            text='The first 100 submissions of any survey are free'
-                        />
-                        <BulletPoint
-                            icon={icons.currencyEuro}
-                            text='4ct. per submission beyond the free tier'
+                        <div className='space-y-3 text-base text-gray-100 flex-col-left'>
+                            <div>
+                                As many surveys as you want - billing per
+                                survey:
+                            </div>
+                            <BulletPoint
+                                icon={icons.discount}
+                                text='The first 100 submissions of any survey are free'
+                            />
+                            <BulletPoint
+                                icon={icons.currencyEuro}
+                                text='4ct. per submission beyond the free tier'
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className='w-full mr-auto lg:max-w-lg flex-col-left'>
+                    <div className='w-full max-w-md mx-auto overflow-hidden rounded-lg'>
+                        <img
+                            src={Gif}
+                            className='w-full h-auto'
+                            alt='Monkey with a cash'
                         />
                     </div>
                 </div>
-            }
-            rightChild={
-                <div className='w-full max-w-md mx-auto overflow-hidden rounded-lg'>
-                    <img
-                        src={Gif}
-                        className='w-full h-auto'
-                        alt='Monkey with a cash'
-                    />
-                </div>
-            }
-        />
+            </div>
+        </div>
     );
 }
