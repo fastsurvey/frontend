d6643028716310136b7eadaa0f0547d67f8f0e9c
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-07-02 01:15:49 +0200

    Add section scroll detection for pricing

--- src/components/landing-section/landing-section-3.tsx
+++ src/components/landing-section/landing-section-3.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {MouseEvent, useRef, useState} from 'react';
 import Gif from '@assets/gifs/money.webp';
 import {icons} from '@assets/icons';
+import useEvent from '../../utilities/event-utils/use-event';
 
 const BulletPoint = (props: {icon: React.ReactNode; text: string}) => (
     <div className='w-full flex-row-left gap-x-2'>
@@ -9,11 +10,33 @@ const BulletPoint = (props: {icon: React.ReactNode; text: string}) => (
     </div>
 );
 export default function LandingSection3() {
+    const ref = useRef<HTMLDivElement>(null);
+    const [scrollState, setScrollState] = useState<
+        'above' | 'within' | 'below'
+    >('above');
+
+    function handleScroll(e: Event) {
+        if (ref.current) {
+            const {y, height} = ref.current.getBoundingClientRect();
+            if (y > 0) {
+                setScrollState('above');
+            } else if (height + y > window.innerHeight) {
+                setScrollState('within');
+            } else {
+                setScrollState('below');
+            }
+        }
+    }
+    useEvent('scroll', handleScroll);
+
     return (
-        <div className='w-full min-h-[110vh] border-b-4 border-gray-800 centering-col'>
+        <div
+            className='w-full h-[200vh] border-b-4 border-gray-800 centering-col'
+            ref={ref}
+        >
             <div
                 className={
-                    'grid w-full px-8 py-16 ' +
+                    'grid w-full px-8 ' +
                     'grid-cols-1 gap-x-0 gap-y-8 ' +
                     'lg:gap-y-0 lg:grid-cols-2 lg:gap-x-8 ' +
                     'xl:gap-x-12'
@@ -29,6 +52,22 @@ export default function LandingSection3() {
                                 As many surveys as you want - billing per
                                 survey:
                             </div>
+                            <BulletPoint
+                                icon={icons.discount}
+                                text='The first 100 submissions of any survey are free'
+                            />
+                            <BulletPoint
+                                icon={icons.currencyEuro}
+                                text='4ct. per submission beyond the free tier'
+                            />
+                        </div>
+                    </div>
+                    <div className='space-y-5 flex-col-left'>
+                        <div className='text-xl text-blue-100 uppercase'>
+                            <strong>Pricing</strong> should be transparent ...
+                        </div>
+                        <div className='space-y-3 text-base text-gray-100 flex-col-left'>
+                            <div>As ma</div>
                             <BulletPoint
                                 icon={icons.discount}
                                 text='The first 100 submissions of any survey are free'
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,4 +1,5 @@
 export {default as backend} from './backend';
+export {default as eventUtils} from './event-utils';
 export {default as reduxUtils} from './redux-utils';
 export {default as formUtils} from './form-utils';
 export {default as pathUtils} from './path-utils';
