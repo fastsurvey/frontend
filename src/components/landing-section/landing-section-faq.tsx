1652a21611cb9f23c76d9f1b183220b96efde327
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-02 13:52:15 +0200

    Finish faq section

--- src/components/landing-section/landing-section-faq.tsx
+++ src/components/landing-section/landing-section-faq.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 
 const faqs = [
     {
-        question: "What's the best thing about Switzerland?",
-        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+        question: 'Where can I add payment information?',
+        answer: 'Right now, our tool is still completely free to use. We want to see how people use it and implement important features before spending time on implementing payment logic.',
     },
     {
-        question: "What's the best thing about Switzerland?",
-        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+        question: 'When it is currently free, how do you make money?',
+        answer: 'We do not have any investors and pay for the servers ourselves.',
     },
     {
-        question: "What's the best thing about Switzerland?",
-        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+        question: 'Will prices be increased in the future?',
+        answer: 'Definitely not! We will never limit an existing feature to any premium plan. If we change anything about the prices we will only decrease them either for everyone, or for survey/accounts with a many submissions.',
     },
     {
-        question: "What's the best thing about Switzerland?",
-        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+        question: 'What features are planned in the future?',
+        answer: 'We have a lot of exciting stuff planned. In September/October 2021 we will realease a public roadmap, where you can see all of our ideas, comment on them and vote for your favorite.',
+    },
+    {
+        question: 'Why is your free plan so generous?',
+        answer: 'In our opinion, it is not great behavior to make users pay for something before they had time to thouroughly test it. Limiting features to expensive plans or having a very tiny "trial"-version are exactly the things we want avoid.',
     },
 ];
 
@@ -39,6 +43,24 @@ export default function LandingSectionFAQ() {
                             </div>
                         ))}
                     </dl>
+                </div>
+                <div className='-m-4 p-4 mt-12 border-[2px] border-dashed border-gray-700 rounded-lg'>
+                    <h3 className='text-lg font-medium leading-6 text-gray-100'>
+                        Any further questions? Something is not working?
+                    </h3>
+                    <div className='mt-2 text-base text-gray-300'>
+                        Please drop us a line under{' '}
+                        <a
+                            href='mailto:info@fastsurvey.de'
+                            target='blank'
+                            rel='noopener noreferrer'
+                            className='text-blue-200 underline font-weight-600'
+                        >
+                            info@fastsurvey.de
+                        </a>
+                        . We will help you and possibly fix any bugs as soon as
+                        possible!
+                    </div>
                 </div>
             </div>
         </div>
