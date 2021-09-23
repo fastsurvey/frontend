57d97f5f7d71a87425e91e00d656a0d736d31588
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-23 12:28:04 +0200

    Close #59

--- src/components/layout/main-content.tsx
+++ src/components/layout/main-content.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import RocketLogo from '@assets/branding/rocket.svg';
-import {pathUtils} from '@utilities';
 
 function MainContent(props: {children: React.ReactNode}) {
     return (
         <React.Fragment>
-            <header className='relative top-0 left-0 w-full bg-gray-200 md:bg-transparent md:absolute flex-row-left'>
+            <header className='relative top-0 left-0 z-20 w-full bg-gray-200 md:bg-transparent md:absolute flex-row-left'>
                 <div className='h-12 m-4 lg:h-16'>
                     <Link to='/' className={'ringable rounded centering-row'}>
                         <img
@@ -20,15 +19,27 @@ function MainContent(props: {children: React.ReactNode}) {
                     </Link>
                 </div>
             </header>
-            <main
-                className={
-                    'w-screen min-h-screen px-2 bg-gray-200 ' +
-                    'pb-12 pt-4 md:pt-12 flex flex-col items-center ' +
-                    'justify-start md:justify-center'
-                }
-            >
-                {props.children}
-            </main>
+            <div className='relative'>
+                <main
+                    className={
+                        'w-screen min-h-screen px-2 bg-gray-200 ' +
+                        'pb-12 pt-4 md:pt-12 flex flex-col items-center ' +
+                        'justify-start md:justify-center z-10'
+                    }
+                >
+                    {props.children}
+                </main>
+                <footer className='absolute bottom-0 left-0 z-0 w-full flex-row-center'>
+                    <div className='py-1 text-sm text-gray-400 font-weight-500'>
+                        {import.meta.env.MODE === 'development' && (
+                            <>development build</>
+                        )}
+                        {import.meta.env.MODE === 'production' && (
+                            <>version - {import.meta.env.VITE_COMMIT_SHA}</>
+                        )}
+                    </div>
+                </footer>
+            </div>
         </React.Fragment>
     );
 }
