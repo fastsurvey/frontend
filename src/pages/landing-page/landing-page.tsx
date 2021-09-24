ebe666475328711282bade54a7b53a4e0e4636ee
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-24 19:12:18 +0200

    Add commit sha to landing page

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -9,12 +9,22 @@ import {
 
 function LandingPage() {
     return (
-        <div className='w-full bg-gray-900 centering-col'>
-            <LandingSection1Intro />
-            <LandingSection2Demo />
-            <LandingSection3Pricing />
-            <LandingSectionFAQ />
-            <LandingSectionLinks />
+        <div className='relative'>
+            <main className='w-full bg-gray-900 centering-col'>
+                <LandingSection1Intro />
+                <LandingSection2Demo />
+                <LandingSection3Pricing />
+                <LandingSectionFAQ />
+                <LandingSectionLinks />
+            </main>
+            <footer className='absolute bottom-0 left-0 z-0 w-full flex-row-center'>
+                <div className='py-1 text-sm text-gray-500 font-weight-500'>
+                    {import.meta.env.MODE === 'development' && <>development</>}
+                    {import.meta.env.MODE === 'production' && (
+                        <>version {import.meta.env.VITE_COMMIT_SHA}</>
+                    )}
+                </div>
+            </footer>
         </div>
     );
 }
