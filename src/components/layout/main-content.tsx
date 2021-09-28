c1e99a9c37ab2a43e682fd464cc907affe0650f4
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-28 18:19:47 +0200

    #62 - step 2

--- src/components/layout/main-content.tsx
+++ src/components/layout/main-content.tsx
@@ -11,7 +11,12 @@ function MainContent(props: {
 }) {
     return (
         <React.Fragment>
-            <header className='relative top-0 left-0 z-40'>
+            <header
+                className={
+                    'relative top-0 left-0 w-full z-40 ' +
+                    (props.darkMode ? 'dark ' : ' ')
+                }
+            >
                 <div className='absolute top-0 left-0 z-40 p-2 m-1 md:ml-5 md:p-0 md:mt-6'>
                     <Link
                         to='/'
@@ -27,8 +32,31 @@ function MainContent(props: {
                         </div>
                     </Link>
                 </div>
+                <div
+                    className={
+                        'absolute top-0 right-0 h-7 m-2 ' +
+                        'flex-row-center flex-nowrap ' +
+                        'bg-gray-200 rounded text-gray-700 ' +
+                        'text-sm overflow-hidden '
+                    }
+                >
+                    {['light', 'auto', 'dark'].map((m: any) => (
+                        <button
+                            onClick={() => props.setDarkModeToggle(m)}
+                            className={
+                                'first:pl-3 last:pr-3 px-3 font-weight-700 ' +
+                                'h-full flex-row-center rounded-sm ' +
+                                (props.darkModeToggle === m
+                                    ? 'text-blue-900 bg-blue-100 '
+                                    : '')
+                            }
+                        >
+                            {m}
+                        </button>
+                    ))}
+                </div>
             </header>
-            <div className='relative'>
+            <div className={'relative ' + (props.darkMode ? 'dark ' : ' ')}>
                 <main
                     className={
                         'w-screen min-h-screen px-2 bg-gray-100 ' +
@@ -47,7 +75,6 @@ function MainContent(props: {
                             <>version {import.meta.env.VITE_COMMIT_SHA}</>
                         )}
                     </div>
-                    <div className='absolute bottom-0 right-0 w-24 h-8 m-2 bg-gray-300 rounded'></div>
                 </footer>
             </div>
         </React.Fragment>
