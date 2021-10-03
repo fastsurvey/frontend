9a7a7640a7c58bfcbae54d28860ad6378db96ed2
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-10-03 23:31:28 +0200

    Improve deploy scripts

--- scripts/deploy-development.sh
+++ scripts/deploy-development.sh
@@ -7,8 +7,6 @@ yarn build
 docker build -t gcr.io/fastsurvey-infrastructure/frontend-dev .
 docker push gcr.io/fastsurvey-infrastructure/frontend-dev:latest
 
-# https://codelabs.developers.google.com/codelabs/cloud-run-deploy/index.html
 gcloud run deploy frontend-dev \
     --image=gcr.io/fastsurvey-infrastructure/frontend-dev:latest \
-    --platform managed \
-    --tag "commit-$VITE_COMMIT_SHA"
+    --platform managed --tag "commit-$VITE_COMMIT_SHA"
--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -6,9 +6,6 @@ yarn build
 docker build -t gcr.io/fastsurvey-infrastructure/frontend .
 docker push gcr.io/fastsurvey-infrastructure/frontend:latest
 
-# https://codelabs.developers.google.com/codelabs/cloud-run-deploy/index.html
 gcloud run deploy frontend \
     --image=gcr.io/fastsurvey-infrastructure/frontend:latest \
-    --platform managed \
-    --no-traffic \
-    --tag "commit-$VITE_COMMIT_SHA"
+    --platform managed --no-traffic --tag "commit-$VITE_COMMIT_SHA"
