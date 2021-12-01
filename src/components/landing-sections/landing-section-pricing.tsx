c8664a47d3106349f20739ee52adaeb694492c89
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-01 03:05:36 +0100

    Fix visual bug

--- src/components/landing-sections/landing-section-pricing.tsx
+++ src/components/landing-sections/landing-section-pricing.tsx
@@ -64,7 +64,7 @@ export default function LandingSectionPricing(props: {
             <div className='grid max-w-3xl grid-cols-1 md:grid-cols-2 gap-x-3 gap-y-3'>
                 <h2
                     className={
-                        'mb-3 text-2xl uppercase text-center max-w-md ' +
+                        'mb-3 text-2xl uppercase text-center max-w-md md:max-w-full ' +
                         'text-blue-200 font-weight-400 col-span-1 md:col-span-2'
                     }
                 >
