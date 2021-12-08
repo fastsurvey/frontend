a80380056ed3e321b53baaa63bebeb519cd9f2b7
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-08 19:58:22 +0100

    Close #85

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export VITE_COMMIT_SHA="$(git rev-parse --short --verify HEAD)"
 yarn build
 
 docker build --platform linux/amd64 -t gcr.io/fastsurvey-infrastructure/frontend .
 docker push gcr.io/fastsurvey-infrastructure/frontend:latest
 
+VITE_COMMIT_SHA="$(git rev-parse --short --verify HEAD)"
 gcloud run deploy frontend \
     --image=gcr.io/fastsurvey-infrastructure/frontend:latest \
     --platform managed --no-traffic --tag "commit-$VITE_COMMIT_SHA"
