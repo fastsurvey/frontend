321637f21913ecba2ca089b0fc7478e333a9cd7e
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-01-02 17:12:37 +0100

    Make server serve both app and documentation build

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 app.disable('x-powered-by');
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname + '/dist')));
+app.use('/dist', express.static(path.join(__dirname + '/dist')));
+app.use(
+    '/documentation',
+    express.static(path.join(__dirname + '/dist/documentation')),
+);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/app/index.html'));
 });
 
 // Start the server
